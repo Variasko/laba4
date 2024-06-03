0e6d1bf0c8090985d235ff08000d43ab7110e16f
Ilya Peunkov <hotmorty1@gmail.com>
2024-06-03 11:37:54 +0300

    1st commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+from sqlalchemy.exc import IntegrityError
+
+# Создание объекта FastAPI
+app = FastAPI()
+
+# Настройка базы данных MySQL
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://isp_is_test:12345@192.168.25.23/isp_is_test"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class Base(DeclarativeBase): ...
+
+# Определение модели SQLAlchemy для пользователя
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), index=True)  # Указываем длину для VARCHAR
+    email = Column(String(100), unique=True, index=True)  # Указываем длину для VARCHAR
+
+# Создание таблиц в базе данных
+Base.metadata.create_all(bind=engine)
+
+# Определение Pydantic модели для пользователя
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
+
+
+# Зависимость для получения сессии базы данных
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Маршрут для получения пользователя по ID
+@app.get("/users/{user_id}", response_model=UserResponse)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+# Маршрут для создания нового пользователя
+@app.post("/users/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = User(name=user.name, email=user.email)
+    try:
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Email already registered")
